99ecf698e6590d77a8535b9a05ea8694febbb380
Fares Ayadi <146492552+FaresAyadi055@users.noreply.github.com>
2025-02-28 11:01:16 +0100

    Update emergency-tracking.vue

--- medease/src/pages/emergency-tracking.vue
+++ medease/src/pages/emergency-tracking.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app style="background-color: #F4F8FB;"></v-app>
   <Navbar />
-  
+  <!--<LocationPicker :apiKey="process.env.VUE_APP_MAPBOX_KEY" :height="'100vh'" :color="'#000000'" :initialLocation="[103.8198, 1.3521]"></LocationPicker>-->
+  <sync-dialog></sync-dialog>
   <Footer />
 </template>
 
 <script setup>
 import Navbar from '@/components/navbar.vue'
 import Footer from '@/components/footer.vue'
+import SyncDialog from '@/components/SyncButton.vue'
+import LocationPicker from '@/components/LocationPicker.vue';
 </script>
 
 <script>
